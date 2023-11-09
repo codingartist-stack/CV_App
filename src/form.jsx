125bf5d17277727a9a1de5ac9c9f578a35282be0
codingartist-stack <121696544+codingartist-stack@users.noreply.github.com>
2023-11-09 13:03:03 -0800

    trying to break down what I need to do

---
+++ src/form.jsx
@@ -0,0 +1,27 @@
+//form
+
+//personal info
+//name
+//title?
+//phone number
+//email
+//website
+//short blurb about self
+
+//education
+//school
+//level
+//years (example 2007 - 2012)
+//ADD
+
+//career summary
+//job title
+//company
+//years (example 2007 - 2012)
+//responsibilities/achievements
+//ADD
+
+//skills
+//ADD
+
+//DISPLAY
