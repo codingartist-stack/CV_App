a12dbba85af57077110174b7a73591cefb716da7
codingartist-stack <121696544+codingartist-stack@users.noreply.github.com>
2023-11-14 10:15:46 -0800

    I simplified the basic information so I can follow it better. I am not longer getting errors, but it's not working. not sure why.

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,28 @@ import BasicInfo from './components/basicInfo';
 BasicInfo;
 
 function App() {
-  const [personInfo, setPersonInfo] = useState({
-    firstName: 'Indigo',
-    lastName: 'Winter',
-    title: 'Historian',
-    phone: '0123456789',
-    email: 'example@mail.com',
-    website: 'example.com',
-    summary:
-      'As a distinguished historian renowned for my deep expertise in unraveling the intricacies of historical narratives. With a passion for uncovering untold stories and exploring the nuances of different eras, I have made significant contributions to the field through meticulous research and insightful analysis. My publications have garnered acclaim for their scholarly rigor, contributing valuable insights to our understanding of the past.',
-  });
+  const [personInfo, setPersonInfo] = useState('');
+
+  const updatePersonalInfo = () => {
+    setPersonInfo((currentPerson) => {
+      return [
+        ...currentPerson,
+        {
+          id: crypto.randomUUID(),
+          firstName: 'Indigo',
+          lastName: 'Winter',
+          title: 'Historian',
+          summary:
+            'As a distinguished historian renowned for my deep expertise in unraveling the intricacies of historical narratives. With a passion for uncovering untold stories and exploring the nuances of different eras, I have made significant contributions to the field through meticulous research and insightful analysis. My publications have garnered acclaim for their scholarly rigor, contributing valuable insights to our understanding of the past.',
+        },
+      ];
+    });
+  };
+  console.log(personInfo);
 
   return (
     <>
-      <BasicInfo setPersonInfo={setPersonInfo} />
+      <BasicInfo updatePersonalInfo={updatePersonalInfo} />
     </>
   );
 }
--- src/components/basicInfo.jsx
+++ src/components/basicInfo.jsx
@@ -1,109 +1,68 @@
 //personal info
 //name
 //title?
-//phone number
-//email
-//website
 //short blurb about self
 
 import { useState } from 'react';
 
-function BasicInfo() {
-  const [personInfo, setPersonInfo] = useState({
-    firstName: 'Indigo',
-    lastName: 'Winter',
-    title: 'Historian',
-    phone: '0123456789',
-    email: 'example@mail.com',
-    website: 'example.com',
-    summary:
-      'As a distinguished historian renowned for my deep expertise in unraveling the intricacies of historical narratives. With a passion for uncovering untold stories and exploring the nuances of different eras, I have made significant contributions to the field through meticulous research and insightful analysis. My publications have garnered acclaim for their scholarly rigor, contributing valuable insights to our understanding of the past.',
-  });
-
-  const handleFirstName = (e) => {
-    const newFirstPerson = { ...personInfo, firstName: e.target.value };
-    setPersonInfo(newFirstPerson);
-  };
-  const handleLastName = (e) => {
-    const newLastName = { ...personInfo, lastName: e.target.value };
-    setPersonInfo(newLastName);
-  };
-  const handleTitle = (e) => {
-    const newTitle = { ...personInfo, title: e.target.value };
-    setPersonInfo(newTitle);
-  };
-
-  const handlePhone = (e) => {
-    const newPhone = { ...personInfo, phone: e.target.value };
-    setPersonInfo(newPhone);
-  };
-  const handleEmail = (e) => {
-    const newEmail = { ...personInfo, phone: e.target.value };
-    setPersonInfo(newEmail);
-  };
-  const handleWebsite = (e) => {
-    const newURL = { ...personInfo, website: e.target.value };
-    setPersonInfo(newURL);
-  };
-  const handleSummary = (e) => {
-    const newSummary = { ...personInfo, summary: e.target.value };
-    setPersonInfo(newSummary);
-  };
+function BasicInfo({ updatePersonalInfo }) {
+  const [firstName, setFirstName] = useState('');
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    if (personInfo === '') return;
+    console.log('something is happening');
+    if (firstName === '') return;
+
+    updatePersonalInfo(firstName);
   };
 
   return (
     <>
       <form onSubmit={handleSubmit}>
+        <h2>Personal Information</h2>
         <fieldset>
-          <legend>Personal Information</legend>
+          <legend htmlFor="firstName">First Name</legend>
           <input
             type="text"
             placeholder="First Name"
-            onChange={handleFirstName}
+            id="firstName"
+            onChange={(e) => setFirstName(e.target)}
           />
+        </fieldset>
+
+        <fieldset>
+          <legend htmlFor="lastName">Last Name</legend>
           <input
             type="text"
             placeholder="Last Name"
-            onChange={handleLastName}
+            id="lastName"
+            // onChange={handleLastName}
           />
+        </fieldset>
+
+        <fieldset>
+          <legend htmlFor="title">Professional Title</legend>
           <input
             type="text"
             placeholder="Professional Title"
-            onChange={handleTitle}
+            id="title"
+            // onChange={handleTitle}
           />
-          <input type="email" placeholder="email" onChange={handleEmail} />
-          <input type="number" placeholder="phone" onChange={handlePhone} />
-          <input
-            type="url"
-            placeholder="url website"
-            onChange={handleWebsite}
-          />
+        </fieldset>
+
+        <fieldset>
+          <legend htmlFor="summary">Summary</legend>
           <input
             type="text"
             placeholder="Professional Summary"
             className="textSummary"
-            onChange={handleSummary}
+            id="summarty"
+            // onChange={handleSummary}
           />
         </fieldset>
-        <button>add</button>
+
+        <button>save</button>
       </form>
-      <div>
-        <h1>
-          {personInfo.firstName} {personInfo.lastName}
-        </h1>
-        <i>{personInfo.title}</i>
-        <ul>
-          <li>{personInfo.phone}</li>
-          <li>{personInfo.email}</li>
-          <li>{personInfo.website}</li>
-        </ul>
-        <p>{personInfo.summary}</p>
-        <button>Edit</button>
-      </div>
     </>
   );
 }
