51c51a88b03bbc0fe8c80517b26d2bb9c0246c16
codingartist-stack <121696544+codingartist-stack@users.noreply.github.com>
2023-11-16 12:03:28 -0800

    looking at the code for a game plan for more information.

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ function App() {
     title: 'Historian',
     summary:
       'As a distinguished historian renowned for my deep expertise in unraveling the intricacies of historical narratives. With a passion for uncovering untold stories and exploring the nuances of different eras, I have made significant contributions to the field through meticulous research and insightful analysis. My publications have garnered acclaim for their scholarly rigor, contributing valuable insights to our understanding of the past.',
+    education: [],
   });
 
   const updateFirstName = (firstName) => {
