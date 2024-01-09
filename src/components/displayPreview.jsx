6efcbccc79cd626a19a5b33c45c82e820fefe147
codingartist-stack <121696544+codingartist-stack@users.noreply.github.com>
2024-01-09 10:23:52 -0800

    fix the font error, added assets in the src for the fonts. deleted file because I felt like it was redundant and added the HTML for the display to the displayPreview file. I think I am going to do the rest of the preview in the same file.

--- src/components/displayPreview.jsx
+++ src/components/displayPreview.jsx
@@ -1,9 +1,15 @@
-import { DisplayBasicInfo } from './displaybasicInfo';
-
 export function DisplayPreview({ personInfo }) {
   return (
     <section className="preview">
-      <DisplayBasicInfo {...personInfo} />
+      <>
+        <h1 key={personInfo.id}>
+          {personInfo.firstName} {personInfo.lastName}
+        </h1>
+        <h2>
+          <i>{personInfo.title}</i>
+        </h2>
+        <p>{personInfo.summary}</p>
+      </>
     </section>
   );
 }
