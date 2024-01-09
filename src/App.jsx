b2218f4f8b903497ce1197bc5ae9b86067c73e11
codingartist-stack <121696544+codingartist-stack@users.noreply.github.com>
2024-01-09 10:27:13 -0800

    added an email to the person

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ function App() {
     summary:
       'As a distinguished historian renowned for my deep expertise in unraveling the intricacies of historical narratives. With a passion for uncovering untold stories and exploring the nuances of different eras, I have made significant contributions to the field through meticulous research and insightful analysis. My publications have garnered acclaim for their scholarly rigor, contributing valuable insights to our understanding of the past.',
     phone: '123.123.1234',
+    email: 'email@email.com',
     education: [
       {
         school: 'University',
