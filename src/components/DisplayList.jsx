8ec60741b1b863a06e9cb7710567ca980182c4d7
codingartist-stack <121696544+codingartist-stack@users.noreply.github.com>
2024-01-11 10:22:10 -0800

    made an if else state for the display so when the array is empty to not create a list

--- src/components/DisplayList.jsx
+++ src/components/DisplayList.jsx
@@ -1,12 +1,16 @@
 export default function DisplayList(props) {
-  return (
-    <>
-      <h3>{props.name}</h3>
-      <ul>
-        {props.list.map((item) => {
-          return <li key={item.id}>{item.title}</li>;
-        })}
-      </ul>
-    </>
-  );
+  if (props.list.length === 0) {
+    return;
+  } else {
+    return (
+      <>
+        <h3>{props.name}</h3>
+        <ul>
+          {props.list.map((item) => {
+            return <li key={item.id}>{item.title}</li>;
+          })}
+        </ul>
+      </>
+    );
+  }
 }
